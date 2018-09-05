41ad86eb6cb7e0025a3fd3e5f8951b8738d40b37
Mohammed El-Serougi <mo.serougi@gmail.com>
2018-09-05 16:40:48 +0200

    Fixed some bugs.

--- app/src/main/java/tooploox/com/song/MainActivity.kt
+++ app/src/main/java/tooploox/com/song/MainActivity.kt
@@ -19,8 +19,6 @@ import org.json.JSONException
 
 class MainActivity : AppCompatActivity()
 {
-    private val localSongs = ArrayList<Song>()
-    private val remoteSongs = ArrayList<Song>()
     private val songsList = ArrayList<Song>()
 
     private val songsListViewAdapter by lazy {
@@ -37,71 +35,40 @@ class MainActivity : AppCompatActivity()
     {
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
 
         songsListView.adapter = songsListViewAdapter
 
         loadLocalSongs()
     }
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
+    private val navigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId)
+        {
             R.id.navigation_home -> {
 
                 loadLocalSongs()
-
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
 
                 loadRemoteSongsByArtist("Taylor")
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-
-                loadLocalAndRemote()
-
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
-    private fun loadRemoteSongsByArtist(artist: String)
-    {
-        Toast.makeText(this, R.string.load_remote_songs, Toast.LENGTH_SHORT).show()
-
-        val api = ApiService.create()
-        api.getSongs(artist).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe({ result ->
-
-            remoteSongs.clear()
-            for (song in result.results) {
-                remoteSongs.add(song)
-            }
-
-            songsList.clear()
-            songsList.addAll(remoteSongs)
-
-            songsListViewAdapter.notifyDataSetChanged()
-
-        }, { error ->
-            error.printStackTrace()
-
-            Toast.makeText(this, R.string.error_load_remote_songs, Toast.LENGTH_SHORT).show()
-        })
-    }
-
     private fun loadLocalSongs()
     {
-        LoadLocalJsonTask().execute("songs-list.json")
+        loadLocalJsonTask().execute("songs-list.json")
     }
 
-    private inner class LoadLocalJsonTask : AsyncTask<String, Unit, Unit>() {
+    private inner class loadLocalJsonTask : AsyncTask<String, Unit, Unit>() {
         // Start a background task to load the JSON file and parse it
         override fun doInBackground(vararg params: String) {
 
-            localSongs.clear()
+            songsList.clear()
 
             val songs = JSONArray(Util.loadJsonFromAsset(applicationContext, params[0]))
             try {
@@ -111,7 +78,7 @@ class MainActivity : AppCompatActivity()
                     var date: Date? = null
                     val dtStart = s.getString("Release Year")
                     if (!dtStart.isNullOrBlank()) {
-                        val format = SimpleDateFormat("yyyy")
+                        val format = SimpleDateFormat("yyyy", Locale.ENGLISH)
                         try {
                             date = format.parse(dtStart)
                         } catch (e: ParseException) {
@@ -121,32 +88,41 @@ class MainActivity : AppCompatActivity()
 
                     // Parse JSON
                     val song = Song(s.getString("Song Clean"), s.getString("ARTIST CLEAN"), date)
-                    localSongs.add(song)
+                    songsList.add(song)
                 }
             }
             catch (e: JSONException) {
                 e.printStackTrace()
             }
 
-            return Unit
+            return
         }
 
         override fun onPostExecute(result: Unit?) {
             super.onPostExecute(result)
 
-            songsList.clear()
-            songsList.addAll(localSongs)
-
             songsListViewAdapter.notifyDataSetChanged()
         }
     }
 
-    private fun loadLocalAndRemote()
+    private fun loadRemoteSongsByArtist(artist: String)
     {
-        songsList.clear()
-        songsList.addAll(localSongs)
-        songsList.addAll(remoteSongs)
+        Toast.makeText(this, R.string.loading_remote_songs, Toast.LENGTH_SHORT).show()
 
-        songsListViewAdapter.notifyDataSetChanged()
+        val api = ApiService.create()
+        api.getSongs(artist).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe({ result ->
+
+            songsList.clear()
+            for (song in result.results) {
+                songsList.add(song)
+            }
+
+            songsListViewAdapter.notifyDataSetChanged()
+
+        }, { error ->
+            error.printStackTrace()
+
+            Toast.makeText(this, R.string.error_load_remote_songs, Toast.LENGTH_SHORT).show()
+        })
     }
 }
